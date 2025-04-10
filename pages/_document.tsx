4ef74d9168401dc75785d78b521053418cd26871
Aaron Perez <aaperez06@gmail.com>
2025-04-10 01:12:43 +0000

    Added Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Document() {
   return (
@@ -9,6 +10,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
